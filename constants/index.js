55e3191862eeca8e938cd09c8a230340852faa07
Danish Iftikhar <danish6712@gmail.com>
2022-02-25 03:12:43 +0200

    todo APP

---
+++ constants/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+// global color consatanst to be used thorughout app. 
+export const Colors = {
+  primary: '#463FAF',
+  secondary: '#E5E6F8',
+  tertiary: '#ddd',
+  quaternary: '#aaa',
+  danger: '#DC143C',
+};
+// storage keys to store data locally in mobile. 
+export const STORAGE_KEYS = {
+  lists: 'myapp_lists',
+  tasks: 'myapp_tasks',
+};
